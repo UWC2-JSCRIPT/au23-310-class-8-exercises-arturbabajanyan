644a0f1356eaec00524e83d675d3ed974b399002
arturbabajanyan <arturbabajanyanv@gmail.com>
2023-12-03 16:58:09 -0500

    Promise is done

--- promise.js
+++ promise.js
@@ -1,14 +1,74 @@
-let myPromise = new Promise(function(resolve, reject) {
+const myPromise = new Promise(function(resolve, reject) {
+  const num = Math.random();
   setTimeout(function() {
-    resolve();
+    if(num > 0.5) {
+      resolve(num);
+    } else {
+      reject(num)
+    }
   }, 1000);
-});
+})
 
-myPromise
-  .then(function() {
-    return 99;
-  })
-  .then(function(number) {
-    console.log(number);
-  });
+myPromise.then(function(result) {
+  console.log(`Success: ${result}`)
+}).catch(function(result) {
+  console.log(`Failure: ${result}`)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let myPromise = new Promise((resolve, reject) {
+//   setTimeout(() => {
+//     let randomNumber = Math.random();
+//     if(randomNumber > 0.5) {
+//       resolve(randomNumber)
+//     } else {
+//       reject(randomNumber)
+//     }
+    
+//   }, 1000);
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let myPromise = new Promise(function(resolve, reject) {
+//   setTimeout(function() {
+//     resolve();
+//   }, 1000);
+// });
+
+// myPromise
+//   .then(function() {
+//     return 99;
+//   })
+//   .then(function(number) {
+//     console.log(number);
+//   });
 
